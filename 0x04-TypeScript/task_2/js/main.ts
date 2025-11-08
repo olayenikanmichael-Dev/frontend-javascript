35c80ea22dee48498c2e39632f958575c13451ed
olayenikanmichael-Dev <olayenikanmichaelolaniyi@gmail.com>
2025-11-08 07:18:34 +0100

    fix

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -45,11 +45,11 @@ function createEmployee(salary: number | string): Director | Teacher {
   return new Director();
 }
 
-function isDirector(employee: Director | Teacher): employee is Director {
+export function isDirector(employee: Director | Teacher): employee is Director {
   return employee instanceof Director;
 }
 
-function executeWork(employee: Director | Teacher): string {
+export function executeWork(employee: Director | Teacher): string {
   if (isDirector(employee)) {
     return employee.workDirectorTasks();
   } else {
@@ -57,8 +57,17 @@ function executeWork(employee: Director | Teacher): string {
   }
 }
 
-// Test logs (optional)
-console.log(executeWork(createEmployee(200)));
-console.log(executeWork(createEmployee(1000)));
+type Subjects = 'Math' | 'History';
 
-export { createEmployee, isDirector, executeWork, Director, Teacher };
+export function teachClass(todayClass: Subjects): string {
+  if (todayClass === 'Math') {
+    return 'Teaching Math';
+  }
+  return 'Teaching History';
+}
+
+// Optional test logs
+console.log(teachClass('Math'));
+console.log(teachClass('History'));
+
+export { createEmployee, Director, Teacher };
